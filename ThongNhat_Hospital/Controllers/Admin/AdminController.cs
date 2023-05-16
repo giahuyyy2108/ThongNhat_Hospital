59431baef86db980de61e18220b870c2587fb0fa
giahuyyy2108 <vuhuy110@gmail.com>
2023-05-16 16:26:43 +0700

    GH : Role Layout

--- ThongNhat_Hospital/Controllers/Admin/AdminController.cs
+++ ThongNhat_Hospital/Controllers/Admin/AdminController.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ThongNhat_Hospital.Controllers.Admin
 {
+    //[Authorize(Roles = "Admin")]
+    [Authorize]
     public class AdminController : Controller
     {
-
-
 
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Role()
+        {
+            return View();
+        }
+
     }
 }
