35c2a2a07aa5cf90c7395c8e335f69f453658762
giahuyyy2108 <vuhuy110@gmail.com>
2023-05-19 11:21:34 +0700

    GH: Email Confirm

--- ThongNhat_Hospital/Areas/Identity/Pages/User/Create.cshtml.cs
+++ ThongNhat_Hospital/Areas/Identity/Pages/User/Create.cshtml.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using ThongNhat_Hospital.Models;
 using static ThongNhat_Hospital.Areas.Identity.Pages.User1.IndexModel;
@@ -16,13 +19,17 @@ namespace ThongNhat_Hospital.Areas.Identity.Pages.User1
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly IEmailSender _emailSender;
+
 
         public CreateModel(
             UserManager<User> userManager,
-            SignInManager<User> signInManager)
+            SignInManager<User> signInManager,
+             IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _emailSender = emailSender;
         }
 
         public class InputModel
@@ -31,9 +38,9 @@ namespace ThongNhat_Hospital.Areas.Identity.Pages.User1
             [Required(ErrorMessage = "Phải nhập tên {0}")]
             public string Hoten { get; set; }
 
-            [Display(Name = "Nhập tên đăng nhập")]
-            [Required(ErrorMessage = "Phải nhập tên {0}")]
-            public string UserName { get; set; }
+            //[Display(Name = "Nhập tên đăng nhập")]
+            //[Required(ErrorMessage = "Phải nhập tên {0}")]
+            //public string UserName { get; set; }
 
             [Display(Name = "Nhập Email")]
             [Required(ErrorMessage = "Phải nhập tên {0}")]
@@ -67,21 +74,32 @@ namespace ThongNhat_Hospital.Areas.Identity.Pages.User1
         public async Task<IActionResult> OnPostAsync()
         {
 
+            String[] parts = Input.Email.Split(new[] { '@' });
+
             Random random = new Random();
 
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
-
-
-            var user = new User { UserName = Input.UserName, Email = Input.Email };
-            var result = await _userManager.CreateAsync(user, GetRandomPassword(9));
-
-
+            string mk = GetRandomPassword(9);
+            var user = new User { UserName = parts[0], Email = Input.Email };
+            var result = await _userManager.CreateAsync(user,mk);
             if(result.Succeeded)
             {
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Page(
+                    "/Account/ConfirmEmail",
+                    pageHandler: "Xác nhận tài khoản",
+                    values: new { area = "Identity", userId = user.Id, code = code},
+                    protocol: Request.Scheme);
+
+                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    $"Hãy xác nhận mail sau để đăng nhập tài khoản <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>Xác nhận</a>.<br>" +
+                    $"Tài khoản: {user.UserName}<br>" +
+                    $"Mật khẩu: {mk}");
+
                 return RedirectToPage("./Index");
             }
             else
