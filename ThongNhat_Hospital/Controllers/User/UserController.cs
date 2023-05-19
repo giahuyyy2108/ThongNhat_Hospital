44b727786e5f943fb9b4578c419346437fe33fab
phuchuy0608 <huykhungz123@gmail.com>
2023-05-19 13:29:52 +0700

    Layoutuser

---
+++ ThongNhat_Hospital/Controllers/User/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThongNhat_Hospital.Controllers.User
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+    
+    }
+}
