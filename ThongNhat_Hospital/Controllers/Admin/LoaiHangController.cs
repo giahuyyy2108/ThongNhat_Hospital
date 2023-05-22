ce68c455d8d136f17938228eef880d49749b2bc2
giahuyyy2108 <vuhuy110@gmail.com>
2023-05-22 09:26:15 +0700

    GH : List LoaiHang

--- ThongNhat_Hospital/Controllers/Admin/LoaiHangController.cs
+++ ThongNhat_Hospital/Controllers/Admin/LoaiHangController.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using ThongNhat_Hospital.Models;
 
 namespace ThongNhat_Hospital.Controllers.Admin
 {
     public class LoaiHangController : Controller
     {
+
+        private readonly DataBaseContext _context;
+        public LoaiHangController(DataBaseContext context) {
+            _context = context;
+        }
         public IActionResult Index()
+        {
+            List<LoaiHang> loaiHangs = _context.LoaiHang
+                .Include(l => l.Name)
+                .ToList();
+            return View(loaiHangs);
+        }
+
+
+
+        public IActionResult Create()
         {
             return View();
         }
