8327351a2ddea6cea4e5f8c421781ea5c1b2cb46
giahuyyy2108 <vuhuy110@gmail.com>
2023-06-12 16:16:03 +0700

    updatae1

---
+++ ThongNhat_Hospital/Models/ViewModel/DonNhanTrongNgayViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ThongNhat_Hospital.Models.ViewModel
+{
+    public class DonNhanTrongNgayViewModel
+    {
+        public int Id { get; set; } = 0;
+
+        public string Id_Phieu { get; set; }
+
+        public string trangthai { get; set;}
+
+        public string capnhat { get; set; }
+
+        public string id_CT { get; set; }
+    }
+}
